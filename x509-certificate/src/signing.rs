5757ea8b69ef28e32ca3cff44119a5245d263a00
nab138 <nab@nabdev.me>
2026-05-27 22:11:15 -0400

    make KeyInfoSigner thread safe

--- x509-certificate/src/signing.rs
+++ x509-certificate/src/signing.rs
@@ -56,7 +56,7 @@ pub trait Sign {
 }
 
 /// A superset of [Signer] and [Sign].
-pub trait KeyInfoSigner: Signer<Signature> + Sign {}
+pub trait KeyInfoSigner: Signer<Signature> + Sign + Send + Sync {}
 
 #[derive(Clone, Debug)]
 pub struct Signature(Vec<u8>);
